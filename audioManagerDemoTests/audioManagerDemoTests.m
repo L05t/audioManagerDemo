f26af6beac5a5de21520e820e92df29e7e2be2ce
L05t <18610075754@163.com>
2017-02-11 11:49:44 +0800

    Initial Commit

---
+++ audioManagerDemoTests/audioManagerDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  audioManagerDemoTests.m
+//  audioManagerDemoTests
+//
+//  Created by 武家成 on 2017/2/11.
+//  Copyright © 2017年 武家成. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface audioManagerDemoTests : XCTestCase
+
+@end
+
+@implementation audioManagerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
